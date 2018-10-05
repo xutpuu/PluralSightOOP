0326e9f6de7d11061adb23253ef863ca725ae1a2
Egor Shatrov <xutpuu@gmail.com>
2018-10-05 23:21:54 +0300

    test add

--- Customer.cs
+++ Customer.cs
@@ -1,13 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ACM.BL
+﻿namespace ACM.BL
 {
     public class Customer
     {
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
 
     }
 }
